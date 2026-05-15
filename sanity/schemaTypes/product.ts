d16ae56927ced93739acb8e9bf8b3267adbe2bea
Naavyen Patel <naavyenpatel@Naavyens-MacBook-Air.local>
2026-05-15 17:06:24 +0100

    Gallery Sorted

--- sanity/schemaTypes/product.ts
+++ sanity/schemaTypes/product.ts
@@ -1,20 +1,39 @@
-export default {
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
   name: 'product',
-  title: 'Products',
+  title: 'Product',
   type: 'document',
+
   fields: [
-    {
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+
+    defineField({
       name: 'category',
       title: 'Category',
       type: 'string',
       options: {
         list: [
           {title: 'Cakes', value: 'cakes'},
-          {title: 'Desserts', value: 'desserts'},
-          {title: 'Pancake Station', value: 'pancake-station'},
+          {title: 'Dessert Tables', value: 'dessert-tables'},
+          {title: 'Live Desserts', value: 'live-desserts'},
           {title: 'Weddings', value: 'weddings'},
         ],
       },
-}
+    }),
+
+    defineField({
+      name: 'featured',
+      title: 'Featured Product',
+      type: 'boolean',
+      initialValue: false,
+    }),
   ],
-}
+})
